f2a4eb1f26bcbd829c4c53518dee3aaf70656c14
Simbamon <sbhan9511@gmail.com>
2023-03-29 21:29:13 +0900

    ms sql added

--- dags/mysql_data_transform.py
+++ dags/mysql_data_transform.py
@@ -6,3 +6,19 @@ from airflow.utils.task_group import TaskGroup
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
 from airflow.hooks.base_hook import BaseHook
 import pandas as pd
+
+default_args = {
+    'owner': 'simbamon',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+@task()
+def get_src_tables():
+    hook = MsSqlHook(mssql_conn_id="ms_sql_server")
+    sql = """select  t.name as table_name  
+     from sys.tables t where t.name in ('DimProduct','DimProductSubcategory','DimProductCategory') """
+    df = hook.get_pandas_df(sql)
+    print(df)
+    tbl_dict = df.to_dict('dict')
+    return tbl_dict
